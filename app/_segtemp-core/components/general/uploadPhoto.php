827cd5619c9bf0c55cc40ba7136b65a505d16fc6
Julián J. Méndez O <menoc.sk27@gmail.com>
2016-08-16 03:39:38 -0600

    Eliminación de archivos innecesarios

--- app/_segtemp-core/components/general/uploadPhoto.php
+++
@@ -1,7 +0,0 @@
-<?php
-  $filename = $_FILES['file']['name'];
-  $destination = '../../../assets/temp/' . $filename;
-  move_uploaded_file( $_FILES['file']['tmp_name'],$destination);
-
-  echo(realpath($destination));
-?>
