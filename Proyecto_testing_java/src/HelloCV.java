8de17071ba494dd7fb1b186dd215765e334f8524
Daniel Troyo <bollackdtg@gmail.com>
2016-08-20 17:35:17 -0600

    4 trozos iniciales implementados.
    Se implementa las siguiente funciones.
    Lectura de video
    Dividirlo en frames.
    Convertir estos frames  a HSV.
    Generar un histograma por cada frame.
    Representar visualmente estos histogramas.

--- Proyecto_testing_java/src/HelloCV.java
+++ Proyecto_testing_java/src/HelloCV.java
@@ -1,40 +1,67 @@
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-import java.awt.image.BufferedImage;
-import  java.awt.image.DataBufferByte;
-import java.io.File;
-import java.lang.Math;
+import java.awt.*;  
+import java.awt.event.*;  
+import javax.swing.*;  
+import java.awt.image.BufferedImage;  
+import  java.awt.image.DataBufferByte;  
+ 
+import java.awt.image.*;  
+import java.io.File;  
+import java.lang.Math;  
+import java.io.IOException;  
+import java.util.*;  
+import javax.imageio.ImageIO;
 
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.highgui.Highgui;
-import org.opencv.highgui.VideoCapture;
-
-public class HelloCV {
-
-	public static void main(String[] args) {
-		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-    	VideoCapture camera = new VideoCapture("Dissolve1-15.mp4");
-    	
-    	if(!camera.isOpened()){	
-    		System.out.println("Error");
-    	}
-    	else {
-    		Mat frame = new Mat();
-    	    while(true){
-    	    	if (camera.read(frame)){
-    	    		System.out.println("Frame Obtained");
-    	    		System.out.println("Captured Frame Width " + 
-    	    		frame.width() + " Height " + frame.height());
-    	    		Highgui.imwrite("camera.jpg", frame);
-    	    		System.out.println("OK");
-    	    		break;
-    	    	}
-    	    }
-    	    
-    	}
-    	camera.release();
-	}
-}
+import org.opencv.core.*;
+import org.opencv.core.Point;
+import org.opencv.imgproc.*;  
+import org.opencv.highgui.Highgui;  
+import org.opencv.highgui.VideoCapture;  
+ 
+public class HelloCV {  
+ 
+    
+    
+    
+  public static void toBufferedImage(Mat m, String name, String format){  
+        int type = BufferedImage.TYPE_BYTE_GRAY;  
+        if ( m.channels() > 1 ) {  
+            type = BufferedImage.TYPE_3BYTE_BGR;  
+        }  
+        int bufferSize = m.channels()*m.cols()*m.rows();  
+        byte [] b = new byte[bufferSize];  
+        m.get(0,0,b); // get all the pixels  
+        BufferedImage image = new BufferedImage(m.cols(),m.rows(), type);  
+        final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();  
+        System.arraycopy(b, 0, targetPixels, 0, b.length);   
+        String nameFile = name+"."+format;  
+          
+        try {  
+      ImageIO.write(image, format, new File(nameFile));  
+      } catch (IOException e) {  
+        // TODO Auto-generated catch block  
+        e.printStackTrace();  
+      }  
+ 
+    }  
+    
+ 
+  public static void main(String[] args) {  
+       
+       
+     System.loadLibrary(Core.NATIVE_LIBRARY_NAME);  
+     VideoSegmenter vidProc = new VideoSegmenter();
+     HistogramProcessor histProc = new HistogramProcessor();
+     
+     LinkedList<Mat> frames = vidProc.splitVideoToFramesHSV("Dissolve1-15 480x320 25 fps.mp4");
+     LinkedList<Mat> g =histProc.calculateHistoOfHueVideo(frames);
+           
+     for (int i=0; i<4; i++){
+    	 String name = "histograma"+Integer.toString(i)+".jpg";
+    	 histProc.drawHistogram(g.get(i), name, 300, 300);
+     }
+           
+           
+ 
+   }  
+}  
+ 
---
+++ Proyecto_testing_java/src/VideoSegmenter.java
@@ -0,0 +1,40 @@
+import org.opencv.core.*;
+import org.opencv.imgproc.*;  
+import org.opencv.highgui.Highgui;  
+import org.opencv.highgui.VideoCapture;  
+import java.util.*;  
+
+public class VideoSegmenter {
+
+	private Imgproc imgProcessor = new Imgproc(); 
+	private int hsvScale = imgProcessor.COLOR_BGR2HSV;  
+	
+	public VideoSegmenter(){
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);  
+	}
+	
+	public LinkedList<Mat> splitVideoToFramesHSV(String filename){
+		VideoCapture capture;
+		LinkedList<Mat> images=new LinkedList();  
+		Mat image = new Mat(); 
+		
+		capture = new VideoCapture(filename);
+		if(capture.isOpened()==false){  
+			System.out.println("Video no leido");  
+		}  
+		int i=0;     
+        while(capture.read(image)){  
+        	
+        	Mat hsvImage = new Mat();  
+	        Mat newrgbImage = new Mat();  
+	           
+	        Imgproc.cvtColor(image, hsvImage, hsvScale); //Transformar a escala hsv
+	        images.add(hsvImage);  
+
+           i++;  
+         }  
+         capture.release(); 
+         return images;
+       
+	}
+}
