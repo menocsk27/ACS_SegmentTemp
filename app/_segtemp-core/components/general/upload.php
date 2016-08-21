1e59155d4b4c82f0a91aee40cbe23bb83da2727b
Julián J. Méndez O <menoc.sk27@gmail.com>
2016-08-21 02:44:55 -0600

    Subida

--- app/_segtemp-core/components/general/upload.php
+++ app/_segtemp-core/components/general/upload.php
@@ -1,29 +1,36 @@
 <?php
-  //$extension = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
-  //echo "Type: " . $_FILES["file"]["type"] . "<br />";
-  //echo "Error: " . $_FILES["file"]["error"] . "<br />";
-  //echo "Size: " . $_FILES["file"]["size"] . "<br />";
-  //echo "Tmp Name: " . $_FILES["file"]["tmp_name"] . "<br />";
-  //echo "Name: " . $_FILES["file"]["name"] . "<br />";
-
+    $allowedExts = array("jpg", "jpeg", "gif", "png", "mp3", "mp4", "wma");
   
-if ($_FILES["file"]["type"] == "video/mp4"){
-      if ($_FILES["file"]["error"] > 0){
-          echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
-      }else{
-          echo "Upload: " . $_FILES["file"]["name"] . "<br />";
-          echo "Type: " . $_FILES["file"]["type"] . "<br />";
-          echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-          echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
-          if (file_exists("upload/" . $_FILES["file"]["name"])){
-              echo $_FILES["file"]["name"] . " already exists. ";
-          }else{
-              $destination = '../../../assets/temp/' . $_FILES["file"]["name"];
-              move_uploaded_file($_FILES["file"]["tmp_name"], $destination);
-              echo "Stored in: " . $destination;
-          }
-      }
-  }else{
-      echo "Invalid file";
-  }
+    echo "Type: " . $_FILES["file"]["type"] . "<br />";
+    echo "Error: " . $_FILES["file"]["error"] . "<br />";
+    echo "Size: " . $_FILES["file"]["size"] . "<br />";
+    echo "Tmp Name: " . $_FILES["file"]["tmp_name"] . "<br />";
+    echo "Name: " . $_FILES["file"]["name"] . "<br />";
+
+    
+    if ((($_FILES["file"]["type"] == "video/mp4")
+      || ($_FILES["file"]["type"] == "audio/mp3")
+      || ($_FILES["file"]["type"] == "audio/wma")
+      || ($_FILES["file"]["type"] == "image/pjpeg")
+      || ($_FILES["file"]["type"] == "image/gif")
+      || ($_FILES["file"]["type"] == "image/jpeg")))
+      {
+        if ($_FILES["file"]["error"] > 0){
+            echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
+        }else{
+            echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+            echo "Type: " . $_FILES["file"]["type"] . "<br />";
+            echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
+            echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
+            if (file_exists("upload/" . $_FILES["file"]["name"])){
+                echo $_FILES["file"]["name"] . " already exists. ";
+            }else{
+                $destination = '../../../assets/temp/' . $_FILES["file"]["name"];
+                move_uploaded_file($_FILES["file"]["tmp_name"], $destination);
+                echo "Stored in: " . $destination;
+            }
+        }
+    }else{
+        echo "Invalid file";
+    }
 ?>
--- app/_segtemp-core/components/home/homeCtrl.js
+++ app/_segtemp-core/components/home/homeCtrl.js
@@ -1,18 +1,53 @@
-app.controller('homeCtrl', function($scope, $http, $location) {
+app.controller('homeCtrl', ['$scope', '$http', 'Upload', '$timeout', '$location', function($scope, $http, Upload, $timeout, $location) {
 
+  $scope.currentVideo = '/app/assets/temp/aw ye.jpg';
+  $scope.videoToUpload = '';
+
+  $scope.uploadVideo = function(file){
+    if (file) {
+      file.upload = Upload.upload({ url: '_segtemp-core/components/general/upload.php', data: { file: file } });
+    }
+  }
+  
   window.onhashchange = function() {
     history.go(0);
   }
-    
+  
+  $("#uploadVideoDiv").on("click", function() {
+    $('#selectVideoInput').click(); 
+  });
+
   $('.dropdown').dropdown({
     transition: 'drop'
   });
 
   $('.dropdown.config').popup({
-    position: 'bottom left',
+    position: 'left center',
     title: 'Documentación',
     target: '.ui.icon.infoIcon',
     content: 'Le presentamos la documentación y a los involucrados en el proyecto'
   });
 
-});
+  $('.uploadIcon').popup({
+    position: 'left center',
+    title: 'Seleccionar video',
+    target: '.ui.icon.infoIcon',
+    content: 'Seleccione el archivo y comienza la subida'
+  });
+
+  $("#uploadVideoInput").click( function() {
+    if($('#selectVideoInput').val() == ""){
+      sweetAlert(
+        'Nope',
+        'Debe especificar un archivo de video para analizar.',
+        'error')    
+    }
+    else{
+      sweetAlert(
+        'Por favor espere',
+        'Su archivo está siendo subido',
+        'warning')
+    }
+  });
+
+}]);
