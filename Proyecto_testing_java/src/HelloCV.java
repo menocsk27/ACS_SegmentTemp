cbde7000b9c3468bd7a08216e381553a7fddd4df
Daniel Troyo <bollackdtg@gmail.com>
2016-08-20 19:38:26 -0600

    Draft de pruebas unitarias JUnit.

--- Proyecto_testing_java/src/HelloCV.java
+++ Proyecto_testing_java/src/HelloCV.java
@@ -1,67 +1,39 @@
-import java.awt.*;  
-import java.awt.event.*;  
-import javax.swing.*;  
-import java.awt.image.BufferedImage;  
-import  java.awt.image.DataBufferByte;  
  
-import java.awt.image.*;  
-import java.io.File;  
-import java.lang.Math;  
-import java.io.IOException;  
 import java.util.*;  
-import javax.imageio.ImageIO;
 
 import org.opencv.core.*;
-import org.opencv.core.Point;
-import org.opencv.imgproc.*;  
-import org.opencv.highgui.Highgui;  
-import org.opencv.highgui.VideoCapture;  
- 
+import org.junit.*;
+import org.junit.runner.notification.*;
+import org.junit.Assert;
 public class HelloCV {  
  
     
+	static VideoSegmenter vidProc;
+    static HistogramProcessor histProc;  
+
+    @Before
+    public void setEnvironment(){
+    	vidProc = new VideoSegmenter();
+        histProc = new HistogramProcessor(); 
+    }
     
-    
-  public static void toBufferedImage(Mat m, String name, String format){  
-        int type = BufferedImage.TYPE_BYTE_GRAY;  
-        if ( m.channels() > 1 ) {  
-            type = BufferedImage.TYPE_3BYTE_BGR;  
-        }  
-        int bufferSize = m.channels()*m.cols()*m.rows();  
-        byte [] b = new byte[bufferSize];  
-        m.get(0,0,b); // get all the pixels  
-        BufferedImage image = new BufferedImage(m.cols(),m.rows(), type);  
-        final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();  
-        System.arraycopy(b, 0, targetPixels, 0, b.length);   
-        String nameFile = name+"."+format;  
-          
-        try {  
-      ImageIO.write(image, format, new File(nameFile));  
-      } catch (IOException e) {  
-        // TODO Auto-generated catch block  
-        e.printStackTrace();  
-      }  
- 
-    }  
-    
- 
-  public static void main(String[] args) {  
-       
-       
-     System.loadLibrary(Core.NATIVE_LIBRARY_NAME);  
-     VideoSegmenter vidProc = new VideoSegmenter();
-     HistogramProcessor histProc = new HistogramProcessor();
-     
-     LinkedList<Mat> frames = vidProc.splitVideoToFramesHSV("Dissolve1-15 480x320 25 fps.mp4");
-     LinkedList<Mat> g =histProc.calculateHistoOfHueVideo(frames);
-           
-     for (int i=0; i<4; i++){
-    	 String name = "histograma"+Integer.toString(i)+".jpg";
-    	 histProc.drawHistogram(g.get(i), name, 300, 300);
-     }
-           
-           
- 
-   }  
+    @Test 
+	public void prueba() {
+		for(int i = 0; i < 5; i++){
+			System.loadLibrary(Core.NATIVE_LIBRARY_NAME);  
+			 
+			 
+	    	LinkedList<Mat> frames = vidProc.splitVideoToFramesHSV("Dissolve1-15 480x320 25 fps.mp4");
+	    	Assert.assertTrue(frames.size()>15);
+	    	
+	    	LinkedList<Mat> g =histProc.calculateHistoOfHueVideo(frames);
+	    	Assert.assertTrue(g.size()>15);
+	    	
+			 for (int j=0; i<4; i++){
+				 String name = "histograma"+Integer.toString(j)+".jpg";
+				 histProc.drawHistogram(g.get(i), name, 300, 300);
+			 }
+		}
+	}
 }  
  
---
+++ Proyecto_testing_java/src/TestRunner.java
@@ -0,0 +1,14 @@
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+import org.junit.runner.*;
+public class TestRunner {
+   public static void main(String[] args) {
+      Result result = JUnitCore.runClasses(HelloCV.class);
+    
+      for (Failure failure : result.getFailures()) {
+         System.out.println(failure.toString());
+      }
+    
+      System.out.println(result.wasSuccessful());
+   }
+}
